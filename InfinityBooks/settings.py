235ffecddf3877fb449a68aa3e78e230d0ce0ea4
cfahim880 <cfahim880@gmail.com>
2024-12-04 21:28:19 +0600

    changes done

--- InfinityBooks/settings.py
+++ InfinityBooks/settings.py
@@ -130,10 +130,10 @@ REST_FRAMEWORK = {
 }
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS=["http://127.0.0.1:5500"]
+CSRF_TRUSTED_ORIGINS=["https://cfahim207.github.io","http://127.0.0.1:5500"]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5501",
+    "https://cfahim207.github.io","http://127.0.0.1:5500"
 ]
 
 
